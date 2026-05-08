ec907ce740bfce118d9b9ec825b0d9f524ad2132
HELLOWORLD5002 <your_real_email@gmail.com>
2026-05-08 13:07:56 +0800

    Fix DB password env var

--- src/util/DBConnection.java
+++ src/util/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
 
     private static final String URL = "jdbc:mysql://" + System.getenv("MYSQLHOST") + ":" + System.getenv("MYSQLPORT") + "/" + System.getenv("MYSQLDATABASE");
     private static final String USER = System.getenv("MYSQLUSER");
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = System.getenv("MYSQLPASSWORD");
 
     public static Connection getConnection() throws SQLException {
         try {
