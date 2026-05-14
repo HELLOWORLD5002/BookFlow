3dffd006ad8c5a99e295c69959b450e45338bdf4
HELLOWORLD5002 <your_real_email@gmail.com>
2026-05-14 14:18:02 +0800

    Fix admin stats

--- src/servlets/AdminDashboardServlet.java
+++ src/servlets/AdminDashboardServlet.java
@@ -27,6 +27,13 @@ public class AdminDashboardServlet extends HttpServlet {
         request.setAttribute("books", bookDAO.getAll());
         request.setAttribute("students", studentDAO.getAll());
         request.setAttribute("penalties", penaltyDAO.getAll());
+        java.util.List books2 = bookDAO.getAll();
+        java.util.List students2 = studentDAO.getAll();
+        request.setAttribute("totalBooks", books2.size());
+        request.setAttribute("availableBooks", books2.stream().filter(b -> !((models.Book)b).isDamaged() && ((models.Book)b).getStock() > 0).count());
+        request.setAttribute("totalStudents", students2.size());
+        request.setAttribute("overdueCount", txDAO.getAll().stream().filter(t -> !((models.Transaction)t).isReturned() && ((models.Transaction)t).getExpectedReturn().isBefore(java.time.LocalDate.now())).count());
+        request.setAttribute("unpaidTotal", 0.0);
 
         request.getRequestDispatcher("/pages/admin_dashboard.jsp").forward(request, response);
     }
