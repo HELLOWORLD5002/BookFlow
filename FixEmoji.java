3738250362352ea6a77a2e20613a33f369d1a2eb
HELLOWORLD5002 <your_real_email@gmail.com>
2026-05-15 10:46:20 +0800

    Fix emoji with Java unicode replacement

---
+++ FixEmoji.java
@@ -0,0 +1,15 @@
+﻿import java.nio.file.*;
+import java.nio.charset.*;
+public class FixEmoji {
+    public static void main(String[] args) throws Exception {
+        String path = "web/pages/admin_dashboard.jsp";
+        String content = new String(Files.readAllBytes(Paths.get(path)), StandardCharsets.UTF_8);
+        content = content.replace("\u00f0\u009f\u0094\u008b", "&#128203;");
+        content = content.replace("\u00f0\u009f\u0094\u009a", "&#128218;");
+        content = content.replace("\u00f0\u009f\u0091\u00a5", "&#128101;");
+        content = content.replace("\u00f0\u009f\u0092\u00b0", "&#128176;");
+        content = content.replace("\u00f0\u009f\u0091\u00a4", "&#128100;");
+        Files.write(Paths.get(path), content.getBytes(StandardCharsets.UTF_8));
+        System.out.println("Done!");
+    }
+}
